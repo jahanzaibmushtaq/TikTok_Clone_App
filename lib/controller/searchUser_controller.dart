e006641620f5fcd8d09244e79b3dfdc1573b7f3e
DESKTOP-6TJN08C\Dell <chzabi197@gmail.com>
2023-08-21 20:13:44 +0500

    this is the first commit.

--- lib/controller/searchUser_controller.dart
+++ lib/controller/searchUser_controller.dart
@@ -1,22 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:tiktok_yt/model/user.dart' ;
 import 'package:get/get.dart';
+import 'package:tiktok_yt/model/user.dart';
 
-class SearchController extends GetxController{
+class SearchController extends GetxController {
   final Rx<List<myUser>> _searchUsers = Rx<List<myUser>>([]);
-  
+
   List<myUser> get searchedUsers => _searchUsers.value;
-  
-  searchUser(String query) async{
+
+  void searchUser(String query) async {
     _searchUsers.bindStream(
-      FirebaseFirestore.instance.collection("users").where("name" , isGreaterThanOrEqualTo: query).snapshots().map((QuerySnapshot queryRes){
+      FirebaseFirestore.instance
+          .collection("users")
+          .where("name", isGreaterThanOrEqualTo: query)
+          .snapshots()
+          .map((QuerySnapshot queryRes) {
         List<myUser> retVal = [];
-        for(var element in queryRes.docs ){
+        for (var element in queryRes.docs) {
           retVal.add(myUser.fromSnap(element));
         }
         return retVal;
-
-      })
+      }),
     );
   }
 }
--- lib/view/screens/search_screen.dart
+++ lib/view/screens/search_screen.dart
@@ -1,63 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_yt/controller/searchUser_controller.dart';
 import 'package:tiktok_yt/model/user.dart';
 import 'package:tiktok_yt/view/screens/profile_screen.dart';
-import 'package:tiktok_yt/view/widgets/text_input.dart';
 import 'package:get/get.dart';
-class SearchScreen extends StatelessWidget {
-   SearchScreen({Key? key}) : super(key: key);
-TextEditingController searchQuery = TextEditingController();
+import 'package:tiktok_yt/controller/searchUser_controller.dart' as MyController;
 
-final SearchController searchController = Get.put(SearchController());
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchQuery = TextEditingController();
+
+  final searchController = Get.put(MyController.SearchController());
+
+  @override
+  void initState() {
+    super.initState();
+    searchController.searchUser('');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Obx(() {
-        return Scaffold(
-          appBar: AppBar(
-
-            title: TextFormField(
-                decoration: new InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    contentPadding:
-                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
-                    hintText: "Search Username"
-                )
-            ,controller: searchQuery , onFieldSubmitted: (value){
+      return Scaffold(
+        appBar: AppBar(
+          title: TextFormField(
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              disabledBorder: InputBorder.none,
+              contentPadding:
+              EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+              hintText: "Search Username",
+            ),
+            controller: searchQuery,
+            onFieldSubmitted: (value) {
               searchController.searchUser(value);
-            },),
-
-
+            },
           ),
-          body:  searchController.searchedUsers.isEmpty ?   Center(
-            child: Text("Search Users!"),
-          ) :
-          ListView.builder(
-              itemCount: searchController.searchedUsers.length,
-              itemBuilder: (context, index){
+        ),
+        body: searchController.searchedUsers.isEmpty
+            ? const Center(
+          child: Text("Search Users!"),
+        )
+            : ListView.builder(
+          itemCount: searchController.searchedUsers.length,
+          itemBuilder: (context, index) {
             myUser user = searchController.searchedUsers[index];
 
             return ListTile(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfileScreen(uid: user.uid)));
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(uid: user.uid),
+                  ),
+                );
               },
               leading: CircleAvatar(
-                backgroundImage: NetworkImage(
-
-                  user.profilePhoto
-                ),
+                backgroundImage: NetworkImage(user.profilePhoto),
               ),
-
               title: Text(user.name),
-
             );
-          })
-          ,
-        );
-      }
-    );
+          },
+        ),
+      );
+    });
   }
 }
